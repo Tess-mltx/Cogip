82459e79122db3dd9a5ff592f70a2bc80f056dce
Konihno <nicolaslong@MacBook-Air-de-Konihno.local>
2024-02-29 17:00:22 +0100

    looog2

--- Model/Compagnies.php
+++ Model/Compagnies.php
@@ -4,13 +4,13 @@ declare(strict_types = 1);
 class Companies 
 {
     public string $name;
-    public string $contry;
+    public string $country;
     public string $tvaNumber;
     public string $created_at;
-    public function __construct(string $name, string $contry, string $tvaNumber, string $created_at)
+    public function __construct(string $name, string $country, string $tvaNumber, string $created_at)
     {
         $this->name = $name;
-        $this->country = $contry;
+        $this->country = $country;
         $this->tvaNumber = $tvaNumber;
         $this->created_at = $created_at;
     }
