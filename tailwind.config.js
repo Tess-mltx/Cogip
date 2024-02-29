06a61e9658ee20012883a2d68c46cea868d7703c
GMKRAKEN23 <giuseppe23@hotmail.com>
2024-02-29 09:45:45 +0100

    add modif header and footer html css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,php}", "./View/**/*.php"],
+  theme: {
+    extend: {
+      colors: {
+        'primary-color-orange': '#FE6402'
+      },
+      spacing: {
+        '543': '543px',
+        '1193': '1193px',
+      },
+      fontFamily: {
+        'sans': ['"Poppins"', ...defaultTheme.fontFamily.sans],
+      }
+    },
+  },
+  plugins: [],
+}
