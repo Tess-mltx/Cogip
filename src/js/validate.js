3ac9b2dc1dd42cabb3dabc675019f723ecc82f99
GMKRAKEN23 <giuseppe23@hotmail.com>
2024-03-13 14:44:42 +0100

    add modif validate.js

--- src/js/validate.js
+++ src/js/validate.js
@@ -1,52 +1,67 @@
 let form = document.querySelector("form");
 
-function ValidateUsername() {
+function validateUsername() {
     let username = document.querySelector('#username').value;
+    let text = document.querySelector('#username + .control .text');
     let pattern = /^[a-zA-Z_-]{4,16}$/; /* min 1 minuscule et 1 majuscule, min. 4 chiffres, max 16 chiffres */
 
     if (username.match(pattern)) {
         text.classList.add('Valid');
         text.classList.remove('invalid');
-        text.innerHTML = "Your Email Address is Valid";
+        text.innerHTML = "Votre nom d'utilisateur est valide";
         text.style.color = "green";
-    }
-
-    else {
+    } else {
         text.classList.remove('Valid'); 
         text.classList.add('invalid'); 
-        text.innerHTML = "* Please Enter Email Address";
+        text.innerHTML = "* Veuillez entrer un nom d'utilisateur valide";
         text.style.color = "red";
     }
-    
-    if (email === "") {
+
+    if (username === "") {
         text.classList.remove('valid');
         text.classList.remove('invalid');
         text.innerHTML = "";
     }
 }
 
-function ValidatePassword() {
+function validatePassword() {
     let password = document.querySelector('#password').value;
+    let text = document.querySelector('#password + .control .text');
     let pattern = /^(?=.*[A-Z])(?=.*\d).{6,}$/; /* min. 1 majuscule, min. 1 chiffre, min. 6 caractères */ 
 
     if (password.match(pattern)) {
         text.classList.add('Valid');
         text.classList.remove('invalid');
-        text.innerHTML = "Your Email Address is Valid";
+        text.innerHTML = "Votre mot de passe est valide";
         text.style.color = "green";
-    }
-
-    else {
+    } else {
         text.classList.remove('Valid'); 
         text.classList.add('invalid'); 
-        text.innerHTML = "* Please Enter Email Address";
+        text.innerHTML = "* Veuillez entrer un mot de passe valide";
         text.style.color = "red";
     }
 
-    if (email === "") {
+    if (password === "") {
         text.classList.remove('valid');
         text.classList.remove('invalid');
         text.innerHTML = "";
     }
 }
 
+function formValidate() {
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        validateUsername();
+        validatePassword();
+
+        if (document.querySelectorAll('.invalid').length > 0) {
+            alert("Veuillez remplir tous les champs requis correctement.");
+            return;
+        }
+
+        form.submit();
+    });
+}
+
+formValidate();
