dde9dbca116c33652826f0bb26c47908c109fb5d
Tess-mltx <maloteaux.stacy@gmail.com>
2024-03-19 10:49:02 +0100

    variable fail test

--- Connect/Cogip.php
+++ Connect/Cogip.php
@@ -1,8 +1,14 @@
 <?php
+// $servername = getenv('DB_HOST');
+// $username = getenv('DB_USERNAME');
+// $password = getenv('DB_PASSWORD');
+// $dbname = getenv('DB_NAME');
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "cogip";
+
 
 try {
     $bdd = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
